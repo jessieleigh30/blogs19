3c4a7b688935fe54140155c4a785eb3504a70d66
Jessica Anderson <jessie.leigh30@gmail.com>
2019-02-18 14:05:27 -0700

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  root "static_pages#home"
+
+  #custom route
+  get "/about", to: "static_pages#about"
+
   resources :pages
 end
